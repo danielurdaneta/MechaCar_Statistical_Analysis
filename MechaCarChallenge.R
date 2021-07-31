bfa50e181dce1ca8a592e30f3ac5f53c90792a57
Daniel Urdaneta <daniel.urdaneta02@gmail.com>
2021-07-31 15:57:56 -0500

    analysis updated

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,4 +20,17 @@ total_summary <- summarize(sus_data, Mean = mean(PSI), Median = median(PSI), Var
 
 lot_summary <- group_by(sus_data, Manufacturing_Lot) %>%
   summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# T test
+
+lot1 = sus_data %>% subset(Manufacturing_Lot == 'Lot1' ,)
+lot2 = sus_data %>% subset(Manufacturing_Lot =='Lot2' ,)
+lot3 = sus_data %>% subset(Manufacturing_Lot == 'Lot3' ,)
+
+generalT <- t.test(sus_data$PSI, mu= 1500)
+lot1T <- t.test(lot1$PSI, mu= 1500)
+lot2T <- t.test(lot2$PSI, mu= 1500)
+lot3T <- t.test(lot3$PSI, mu= 1500)
+
+
   
